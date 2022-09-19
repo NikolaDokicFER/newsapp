8ad4d623e26bf7b466dccaedac1b91b65e2777e0
Nikola Đokić <nd51917@fer.hr>
2022-09-19 19:57:29 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Karlo Tomašić on 19.09.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
